be47cd50ace2a51576da72cc498456ca412d6ebc
co2ma <window3357@gmail.com>
2026-02-28 21:37:56 +0900

    add: 최초 파일 추가

---
+++ src/test/java/com/co2ma/mini_proj/MiniProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co2ma.mini_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
